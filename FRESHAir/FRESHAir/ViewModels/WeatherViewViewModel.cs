a4e980b547137c6a18a5fbef213757f44bbe5254
Royce551 <poohwie2005@gmail.com>
2023-09-07 20:42:13 -0500

    Basic formatting for Day Ahead

--- FRESHAir/FRESHAir/ViewModels/WeatherViewViewModel.cs
+++ FRESHAir/FRESHAir/ViewModels/WeatherViewViewModel.cs
@@ -1,7 +1,9 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia.Data.Converters;
+using CommunityToolkit.Mvvm.ComponentModel;
 using FRESHAir.Services.WeatherService;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,8 +38,34 @@ namespace FRESHAir.ViewModels
 
         public async Task Initialize()
         {
-            CurrentWeatherSummary = await WeatherService.GetWeatherSummaryAsync("McMurdo Station", TemperatureScale.Fahrenheit);
+            CurrentWeatherSummary = await WeatherService.GetWeatherSummaryAsync("Opelousas", TemperatureScale.Fahrenheit);
         }
 
+    }
+
+    public class TemperatureToStringConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is null) return null;
+            if (value is Temperature temperature)
+            {
+                var decimalSymbol = temperature.Scale switch
+                {
+                    TemperatureScale.Celsius => "°C",
+                    TemperatureScale.Fahrenheit => "°F",
+                    TemperatureScale.Kelvin => "°K",
+                    _ => throw new Exception()
+                };
+
+                return $"{temperature.Value:F1}{decimalSymbol}";
+            }
+            else throw new ArgumentException("Converted a non-temperature with the TemperatureToStringConverter");
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
